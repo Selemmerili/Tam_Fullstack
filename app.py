acec297b08dbc604ce6ed014a8bbe89e3851f204
Y4N1S <76550760+Y4N1S@users.noreply.github.com>
2021-03-10 12:04:54 +0100

    Add files via upload
    docstring

--- app.py
+++ app.py
@@ -16,25 +16,49 @@ def home():
     stations()
     return render_template('home.html',title='Welcome')
 
+
 @app.route('/stations')
 def entry_point():
     return jsonify(stations())
  
    
-
-
 @app.route('/next/<station>')
 def demande(station):
+    """ This function searches for the next trains to come
+    at a set station no matter the line nor destination.
+    Parameters : town, station (ex: Montpellier, BELEVEDERE)
+    Returns : json data
+    Example : http://127.0.0.1:5000/Montpellier/stations/BELVEDERE
+    [ [ [ "10", "BELVEDERE", "AIGUELONGUE", "Montpellier" ],
+    [ "20:06:10", "20:36:10", "19:50:13" ] ], ...
+    [ [ "15", "BELVEDERE", "SABINES", "Montpellier" ],
+    [ "20:21:47", "20:50:51" ] ] ]
+    """
     return jsonify(next_transports(station))
 
 @app.route('/line')
 def ligne_list():
-    # jsonify(stations())
-    # return render_template('./app.html')
+    """ This function searches for every train stops
+    for a set town.
+    Parameter : town (ex: Montpellier)
+    Returns : json data
+    Example : http://127.0.0.1:5000/Montpellier/stations
+    [ "A. BROUSSONET", "A. L. JUSSIEU", "AGROPOLIS", ...,
+    "ZI MARBRERIE", "ZONE ARTISANALE", "ZOO" ]
+    """
     return jsonify(Ligne())    
 
 @app.route('/line/<ligne>')
 def demande_ligne(ligne):
+    """ This function searches for the next trains to come
+    at a set station considering a specific line and destination.
+    Parameters : town, station, destination (ex: Montpellier, COMEDIE, SABINES)
+    Returns : json data
+    Example :
+    http://127.0.0.1:5000/Montpellier/next?line=2&station=COMEDIE&destination=SABINES
+    [ [ "2", "COMEDIE", "SABINES", "19:46:09", "Montpellier" ],
+    [ "2", "COMEDIE", "SABINES", "19:58:21", "Montpellier" ] ]
+    """ 
     return jsonify(ligne_search(ligne))
 
 @app.route('/next', methods=['GET'])
